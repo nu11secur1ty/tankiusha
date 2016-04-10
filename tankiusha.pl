b4378bf48b580379d4e11eb5ed46eaec876c7842
nu11secur1ty <systadmi@abv.bg>
2016-04-10 10:05:34 +0300

    Update and rename .gitignore to tankiusha.pl

---
+++ tankiusha.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl 
+#############################
+# copyright @ nu11secur1ty  #   
+#############################
+use strict;
+use warnings;
+use 5.010;
+
+print "Please input, the name of file, who you want to search:\n";
+my $sr_ = <STDIN>;
+my $path = `~/$sr_ /var/$sr_ /etc/$sr_ /usr/$sr_ 
+                       /bin/$sr_ /boot/$sr_ /dev/$sr_ /home/$sr_ 
+                       /lib/$sr_ /lib64/$sr_ /lost+found/$sr_ /media/$sr_ 
+                       /mnt/$sr_ /opt/$sr_ /proc/$sr_ /sbin/$sr_ /seliux/$sr_ 
+                       /srv/$sr_ /sys/$sr_ /tmp/$sr_`;
+my $inodes = ' ~/, /var/, /etc/, /usr/
+                       /bin/, /boot/, /dev/, /home/
+                       /lib/, /lib64/, /lost+found/, /media/
+                       /mnt/, /opt/, /proc/, /sbin/, /seliux/
+                       /srv/, /sys/, /tmp/'; 
+   if ( $sr_ =~ $path ) {
+      print   "Result for $sr_\n";
+      print   "IMPORTANT: 
+                   If do not see, any of those inodes $inodes\n" ;
+      print   "That mean, file you are looking for  'EXIST'  on your system, in 'inode' who you don't see!\n";
+                 
+} 
+
+       
