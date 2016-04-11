a209a5df70538faad44df8489b209bba3ab49336
nu11secur1ty <systadmi@abv.bg>
2016-04-11 00:08:56 -0700

    Update tankiusha.pl

--- tankiusha.pl
+++ tankiusha.pl
@@ -8,12 +8,12 @@ use 5.010;
 
 print "Please input, the name of file, who you want to search:\n";
 my $sr_ = <STDIN>;
-my $path = `~/$sr_ /var/$sr_ /etc/$sr_ /usr/$sr_ 
-                       /bin/$sr_ /boot/$sr_ /dev/$sr_ /home/$sr_ 
-                       /lib/$sr_ /lib64/$sr_ /lost+found/$sr_ /media/$sr_ 
-                       /mnt/$sr_ /opt/$sr_ /proc/$sr_ /sbin/$sr_ /seliux/$sr_ 
-                       /srv/$sr_ /sys/$sr_ /tmp/$sr_`;
-my $inodes = ' ~/, /var/, /etc/, /usr/
+my $path = `~/*/$sr_ /var/*/$sr_ /etc/*/$sr_ /usr/*/$sr_ 
+                       /bin/*/$sr_ /boot/*/$sr_ /dev/*/$sr_ /home/*/$sr_ 
+                       /lib/*/$sr_ /lib64/*/$sr_ /lost+found/*/$sr_ /media/*/$sr_ 
+                       /mnt/*/$sr_ /opt/*/$sr_ /proc/*/$sr_ /sbin/*/$sr_ /seliux/*/$sr_ 
+                       /srv/*/$sr_ /sys/*/$sr_ /tmp/*/$sr_`;
+                        my $inodes = ' ~/, /var/, /etc/, /usr/
                        /bin/, /boot/, /dev/, /home/
                        /lib/, /lib64/, /lost+found/, /media/
                        /mnt/, /opt/, /proc/, /sbin/, /seliux/
@@ -25,5 +25,6 @@ my $inodes = ' ~/, /var/, /etc/, /usr/
       print   "That mean, file you are looking for  'EXIST'  on your system, in 'inode' who you don't see!\n";
                  
 } 
+
 
        
