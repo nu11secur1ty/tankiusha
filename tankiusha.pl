7f4cfcc8d82e48597000e2de91a06d57e839f4e8
nu11secur1ty <systadmi@abv.bg>
2016-11-20 14:55:02 +0200

    Update tankiusha.pl

--- tankiusha.pl
+++ tankiusha.pl
@@ -5,16 +5,27 @@
 use strict;
 use warnings;
 use 5.010;
-
+print "\n\n\n";
 if ( $< != 0 ) {
 print "Must be run as root, using su -:\n"; 
 print "and run again\n";
 exit (0);
 }
+   print "WELCOME TO TANKIUSHA:\n\n\n";
+   print "If you want to search for this file in all machine,\n please use '*' after you type the file name!\n";
+   print "Example: myfile*\n\n\n";
    print "Please input, the name of file, who you want to search:\n";
 my $sr_ = <STDIN>;
+   print "Please wait...\n";
 my $path = `find / -iname $sr_`;
    print  "$path\n";
+
+       
+
+
+
+
+       
 
        
 
